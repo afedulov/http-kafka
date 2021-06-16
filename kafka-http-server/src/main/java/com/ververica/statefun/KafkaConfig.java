dc426ea6dd41b2b26327a025cd0bd98ad7dd282f
austin ce <austin.cawley@gmail.com>
2021-06-16 14:22:29 -0400

    fix(server): add kafka deserializer for v1alpha1 invocation

--- kafka-http-server/src/main/java/com/ververica/statefun/KafkaConfig.java
+++ kafka-http-server/src/main/java/com/ververica/statefun/KafkaConfig.java
@@ -4,11 +4,16 @@ import com.ververica.statefun.reqreply.DefaultReplyingKafkaTemplatePool;
 import com.ververica.statefun.reqreply.StatefunReplyingKafkaTemplatePool;
 import com.ververica.statefun.reqreply.V1Alpha1PayloadCorrelationIdStrategy;
 import com.ververica.statefun.reqreply.v1alpha1.V1Alpha1Invocation;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
 public class KafkaConfig {
@@ -19,6 +24,19 @@ public class KafkaConfig {
 	public StatefunReplyingKafkaTemplatePool<String, byte[], byte[]> replyingKafkaTemplatePool(ProducerFactory<String, byte[]> pf,
 																							   ConcurrentKafkaListenerContainerFactory<String, byte[]> factory) {
 		return new DefaultReplyingKafkaTemplatePool<>(groupId, pf, factory);
+	}
+
+	@Bean
+	public ConsumerFactory<String, V1Alpha1Invocation> consumerFactory(KafkaProperties properties) {
+		return new DefaultKafkaConsumerFactory<>(properties.buildConsumerProperties(), new StringDeserializer(),
+			new JsonDeserializer<>(V1Alpha1Invocation.class));
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, V1Alpha1Invocation> factory(ConsumerFactory<String, V1Alpha1Invocation> consumerFactory) {
+		var containerFactory = new ConcurrentKafkaListenerContainerFactory<String, V1Alpha1Invocation>();
+		containerFactory.setConsumerFactory(consumerFactory);
+		return containerFactory;
 	}
 
 	@Bean
